0079baaa96986df9df2d9d6f20a1a3b7936e857f
XINJIE ZHANG <90270245+dwdwdw5163@users.noreply.github.com>
2025-01-09 17:48:35 +0800

    Update timetagger.cpp

--- src/timetagger.cpp
+++ src/timetagger.cpp
@@ -9,11 +9,11 @@
 TT::TT() {
   t = createTimeTagger();
   t->setTriggerLevel(1, 0.1);
-  t->setTriggerLevel(2, -0.1);
-  t->setTriggerLevel(3, -0.1);
-  t->setTriggerLevel(4, -0.1);
-  t->setTriggerLevel(5, -0.1);
-  t->setTriggerLevel(6, -0.1);
+  t->setTriggerLevel(2, -0.06);
+  t->setTriggerLevel(3, -0.06);
+  t->setTriggerLevel(4, -0.06);
+  t->setTriggerLevel(5, -0.06);
+  t->setTriggerLevel(6, -0.06);
 
 
   c1 = std::make_unique<Correlation>(t, 2, 1, 50, 500);
