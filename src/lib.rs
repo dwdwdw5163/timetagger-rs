1df1132d3b15d841357643163ef7f0da5cc1793a
Xinjie ZHANG <zhang@Xinjies-Laptop.local>
2025-02-04 17:21:21 +0800

    mutable ref

--- src/lib.rs
+++ src/lib.rs
@@ -12,16 +12,15 @@ pub mod ffi {
         pub type Counter;
 
         pub fn TTcreateTimeTaggerNetwork(address: &CxxString) -> UniquePtr<TimeTaggerNetwork>;
-        pub fn TTfreeTimeTaggerNetwork(t: &TimeTaggerNetwork);
-        pub fn TTsetTriggerLevel(t: &TimeTaggerNetwork, channel: i32, level: f64);
-        pub fn TTcreateCorrelation(t: &TimeTaggerNetwork, channel1: i32, channel2: i32, bin_width: i32, max_count: i32) -> UniquePtr<Correlation>;
-        pub fn CorrelationGetData(c: &Correlation) -> UniquePtr<CxxVector<i32>>;
-        pub fn TTcreateCounter(t: &TimeTaggerNetwork, channels: &CxxVector<i32>, bin_width: f64, max_count: i32) -> UniquePtr<Counter>;
-        pub fn CounterGetData(c: &Counter) -> UniquePtr<CxxVector<i32>>;
-        pub fn CorrelationStart(c: &Correlation);
-        pub fn CorrelationStartFor(c: &Correlation, capture_duration: i64, clear: bool);
-        pub fn CorrelationStop(c: &Correlation);
-        pub fn CorrelationWaitUntilFinished(c: &Correlation, timeout: i64) -> bool;
+        pub fn TTsetTriggerLevel(t: &mut TimeTaggerNetwork, channel: i32, level: f64);
+        pub fn TTcreateCorrelation(t: &mut TimeTaggerNetwork, channel1: i32, channel2: i32, bin_width: i32, max_count: i32) -> UniquePtr<Correlation>;
+        pub fn CorrelationGetData(c: &mut Correlation) -> UniquePtr<CxxVector<i32>>;
+        pub fn TTcreateCounter(t: &mut TimeTaggerNetwork, channels: &CxxVector<i32>, bin_width: f64, max_count: i32) -> UniquePtr<Counter>;
+        pub fn CounterGetData(c: &mut Counter) -> UniquePtr<CxxVector<i32>>;
+        pub fn CorrelationStart(c: &mut Correlation);
+        pub fn CorrelationStartFor(c: &mut Correlation, capture_duration: i64, clear: bool);
+        pub fn CorrelationStop(c: &mut Correlation);
+        pub fn CorrelationWaitUntilFinished(c: &mut Correlation, timeout: i64) -> bool;
     }
 }
 
